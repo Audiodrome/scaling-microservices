33c4cfd0dabebaa1ff219e372582b680ac6e4b44
Audiodrome <lien.daniel.h@gmail.com>
2017-12-11 22:26:06 -0800

    [Add] returns unix timestamp

--- server.js
+++ server.js
@@ -3,26 +3,32 @@
 const Hapi = require('hapi');
 
 const server = Hapi.server({
-    host: 'localhost',
-    port: 8000
+  host: 'localhost',
+  port: 8000
 });
 
 server.route({
-    method: 'GET',
-    path: '/api/timestamp',
-    handler: function (request, h) {
-        return 'return unix timestamp';
+  method: 'GET',
+  path: '/api/timestamp/{date}',
+  handler: function (request, h) {
+    const unixTimestamp = { 'unix': Date.parse(request.params.date) };
+    if (!unixTimestamp.unix) {
+      unixTimestamp['natural'] = null;
+    } else {
+      unixTimestamp['natural'] = request.params.date;
     }
+    return unixTimestamp;
+  }
 });
 
 async function start() {
-    try {
-        await server.start();
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    console.log('Server running at:', server.info.uri);
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log('Server running at:', server.info.uri);
 };
 
 start();
